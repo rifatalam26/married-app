2047a79d52110f1abfe0d14a950b0e33bcb1fdca
Md Rifat Alam <mdrftlm@gmail.com>
2025-04-16 19:53:57 +0600

    login screen

--- lib/View/Login screens/login_screen.dart
+++ lib/View/Login screens/login_screen.dart
@@ -10,23 +10,85 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
         child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Text(
+          const Text(
             "Welcome back !",
             style: TextStyle(
                 fontSize: 27,
                 fontWeight: FontWeight.bold,
                 color: Color(0xff252C32)),
           ),
-          Text(
+          const Text(
             "Stay signed in with your account to",
             style: TextStyle(fontSize: 15, color: Color(0xff6B7280)),
           ),
-          Text(
+          const Text(
             "make easier",
             style: TextStyle(fontSize: 15, color: Color(0xff6B7280)),
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          const Row(
+            children: [
+              SizedBox(
+                width: 13,
+              ),
+              Text(
+                "Email",
+                style: TextStyle(
+                  fontSize: 17,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 14,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 15, right: 15),
+            child: TextFormField(
+              decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    // borderSide: BorderSide(color: )
+                  ),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8))),
+            ),
+          ),
+          const SizedBox(
+            width: 35,
+          ),
+          const Row(
+            children: [
+              SizedBox(
+                width: 13,
+              ),
+              Text(
+                "Password",
+                style: TextStyle(
+                  fontSize: 17,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 14,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 15, right: 15),
+            child: TextFormField(
+              decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    // borderSide: BorderSide(color: )
+                  ),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8))),
+            ),
           )
         ]),
       ),

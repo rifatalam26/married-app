5cba10a28d2b9ff614b29c66e32eb05534df92ef
Md Rifat Alam <mdrftlm@gmail.com>
2025-04-16 21:29:03 +0600

    login screen ok

--- lib/View/Login screens/login_screen.dart
+++ lib/View/Login screens/login_screen.dart
@@ -13,6 +13,9 @@ class _LoginScreenState extends State<LoginScreen> {
     return Scaffold(
       body: Center(
         child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          SizedBox(
+            height: 50,
+          ),
           const Text(
             "Welcome back !",
             style: TextStyle(
@@ -29,7 +32,7 @@ class _LoginScreenState extends State<LoginScreen> {
             style: TextStyle(fontSize: 15, color: Color(0xff6B7280)),
           ),
           const SizedBox(
-            height: 50,
+            height: 45,
           ),
           const Row(
             children: [
@@ -104,13 +107,91 @@ class _LoginScreenState extends State<LoginScreen> {
               ),
               Text(
                 "Forgot password?",
-                style: TextStyle(
-                  fontSize: 17,
-                  color: Color(0xff7DBABB)
-                ),
+                style: TextStyle(fontSize: 17, color: Color(0xff7DBABB)),
               )
             ],
-          )
+          ),
+          const SizedBox(
+            height: 45,
+          ),
+          Container(
+            height: 53,
+            width: 331,
+            decoration: BoxDecoration(
+                color: const Color(0xff7DBABB),
+                borderRadius: BorderRadius.circular(8)),
+            child: const Center(
+              child: Text(
+                "Log-in",
+                style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 48),
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 15),
+                  child: Container(
+                    height: 2,
+                    width: 100,
+                    color: const Color(0xffA5ADBA),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    "Or",
+                    style: TextStyle(
+                      fontSize: 19,
+                      color: Color(0xffA5ADBA),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 15),
+                  child: Container(
+                    height: 2,
+                    width: 100,
+                    color: const Color(0xffA5ADBA),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Container(
+            height: 54,
+            width: 280,
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/image/Frame 17559.png"))),
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left: 60),
+            child: Row(
+              children: [
+                Text(
+                  "You don’t Have an account?",
+                  style: TextStyle(fontSize: 14, color: Color(0xff6B7280)),
+                ),
+                Text("  Sign Up",
+                    style: TextStyle(fontSize: 14, color: Color(0xff7DBABB))),
+              ],
+            ),
+          ),
         ]),
       ),
     );

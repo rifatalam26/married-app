38e8a154b0e32bcf254b630768d5519c05b51e8c
Md Rifat Alam <mdrftlm@gmail.com>
2025-04-25 22:28:03 +0600

    wedding_ceremony_music_page1.dart

--- lib/View/Home screens/wedding_ceremony_music_page1.dart
+++ lib/View/Home screens/wedding_ceremony_music_page1.dart
@@ -4,37 +4,78 @@ class WeddingCeremonyMusicPage1 extends StatefulWidget {
   const WeddingCeremonyMusicPage1({super.key});
 
   @override
-  State<WeddingCeremonyMusicPage1> createState() => _WeddingCeremonyMusicPage1State();
+  State<WeddingCeremonyMusicPage1> createState() =>
+      _WeddingCeremonyMusicPage1State();
 }
 
 class _WeddingCeremonyMusicPage1State extends State<WeddingCeremonyMusicPage1> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Row(
-            children: [
-              IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.arrow_back,
-                    size: 30,
-                  )),
-              const SizedBox(
-                width: 8,
-              ),
-              const Text(
-                "Wedding Ceremony Music",
-                style: TextStyle(
-                    fontSize: 22,
-                    fontWeight: FontWeight.bold,
-                    color: Color(0xff141414)),
-              )
-            ],
-          )
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              children: [
+                IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.arrow_back,
+                      size: 30,
+                    )),
+                const SizedBox(
+                  width: 8,
+                ),
+                const Text(
+                  "Wedding Ceremony Music",
+                  style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xff141414)),
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            const Text(
+              "“Simply enter your choices below & start to organise",
+              style: TextStyle(fontSize: 15, color: Color(0xff7F7F7F)),
+            ),
+            const Text(
+              "your key ceremony music, if you don’t want to do ",
+              style: TextStyle(fontSize: 15, color: Color(0xff7F7F7F)),
+            ),
+            const Text(
+              "something, simply say NA (not applicable)”",
+              style: TextStyle(fontSize: 15, color: Color(0xff7F7F7F)),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  height: 40,
+                  width: 100,
+                  decoration: BoxDecoration(
+                      color: Color(0xff7DBABB),
+                      borderRadius: BorderRadius.circular(8)),
+                  child: const Center(
+                    child: Text(
+                      "Add List",
+                      style: TextStyle(fontSize: 16, color: Color(0xffFFFFFF)),
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }

36417632847ef67c2797464f106a636bd2a8128c
Md Rifat Alam <mdrftlm@gmail.com>
2025-04-28 19:31:43 +0600

    wedding_ceremony_music page 1

--- lib/View/Home screens/wedding_ceremony_music_page1.dart
+++ lib/View/Home screens/wedding_ceremony_music_page1.dart
@@ -102,7 +102,7 @@ class _WeddingCeremonyMusicPage1State extends State<WeddingCeremonyMusicPage1> {
               ),
               TextFormField(
                 decoration: InputDecoration(
-                    //suffixIcon: Icons.vector,
+                    suffixIcon: const Icon(Icons.link),
                     hintText: "Song Title & Artist.",
                     hintStyle: TextStyle(color: Colors.grey.shade400),
                     focusedBorder: OutlineInputBorder(
@@ -129,7 +129,7 @@ class _WeddingCeremonyMusicPage1State extends State<WeddingCeremonyMusicPage1> {
               ),
               TextFormField(
                 decoration: InputDecoration(
-                    //suffixIcon: Icons.vector,
+                    suffixIcon: const Icon(Icons.link),
                     hintText: "Song Title & Artist.",
                     hintStyle: TextStyle(color: Colors.grey.shade400),
                     focusedBorder: OutlineInputBorder(
@@ -156,7 +156,7 @@ class _WeddingCeremonyMusicPage1State extends State<WeddingCeremonyMusicPage1> {
               ),
               TextFormField(
                 decoration: InputDecoration(
-                    //suffixIcon: Icons.vector,
+                    suffixIcon: const Icon(Icons.link),
                     hintText: "Song Title & Artist.",
                     hintStyle: TextStyle(color: Colors.grey.shade400),
                     focusedBorder: OutlineInputBorder(

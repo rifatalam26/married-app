d7c3a08ae8268f14c7ac6453a26a5bcd597afa89
Md Rifat Alam <mdrftlm@gmail.com>
2025-05-06 22:01:51 +0600

    wedding_ceremony_music_page5.dart

--- lib/View/Home screens/wedding_ceremony_music_page5.dart
+++ lib/View/Home screens/wedding_ceremony_music_page5.dart
@@ -15,6 +15,7 @@ class _WeddingCeremonyMusicPage4State extends State<WeddingCeremonyMusicPage5> {
       body: Padding(
         padding: const EdgeInsets.all(10),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             const SizedBox(
               height: 60,
@@ -51,11 +52,59 @@ class _WeddingCeremonyMusicPage4State extends State<WeddingCeremonyMusicPage5> {
                   width: 25,
                   decoration: const BoxDecoration(
                       image: DecorationImage(
-                          image: AssetImage(
-                              "assets/image/delete2.png"))),
+                          image: AssetImage("assets/image/delete2.png"))),
                 )
               ],
-            )
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Text(
+              "Flower Girls/Bridesmaids Entrance/Aisle Song",
+              style: TextStyle(fontSize: 15, color: Color(0xff252C32)),
+            ),
+            const Text(
+              "before bride song (Starting Song)",
+              style: TextStyle(fontSize: 15, color: Color(0xff252C32)),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              height: 65,
+              width: 330,
+              decoration: BoxDecoration(
+                  // color: Colors.blue,
+                  borderRadius: BorderRadius.circular(13),
+                  border: const Border(
+                      top: BorderSide(color: Color(0xffE0E0E0), width: 2),
+                      bottom: BorderSide(color: Color(0xffE0E0E0), width: 2),
+                      left: BorderSide(color: Color(0xffE0E0E0), width: 2),
+                      right: BorderSide(color: Color(0xffE0E0E0), width: 2))),
+              child: const Row(
+                children: [
+                  SizedBox(
+                    width: 12,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Text(
+                        "Red- blue",
+                        style: TextStyle(fontSize: 15, color: Color(0xff121212)),
+                      ),
+                      Text(
+                        "http:// song link here.com",
+                        style: TextStyle(fontSize: 15, color: Color(0xff7DBABB)),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),

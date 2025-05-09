8da9a99f7da2ff5e46774ce33a2a366e1d10d774
Md Rifat Alam <mdrftlm@gmail.com>
2025-05-09 17:22:50 +0600

    profile page 2 stack

---
+++ lib/View/Profile screens/profile_page2.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage2 extends StatefulWidget {
+  const ProfilePage2({super.key});
+
+  @override
+  State<ProfilePage2> createState() => _ProfilePage2State();
+}
+
+class _ProfilePage2State extends State<ProfilePage2> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:Column(
+        children: [
+          const SizedBox(
+            height: 50,
+          ),
+          Row(
+            children: [
+              IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.arrow_back,
+                    size: 30,
+                  )),
+              const SizedBox(
+                width: 8,
+              ),
+              const Text(
+                "Profile",
+                style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xff141414)),
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                children: [
+                  Stack(
+                    children: [
+                      CircleAvatar(
+                        radius: 60,
+                        //backgroundColor: Colors.blue,
+                        backgroundImage:
+                        AssetImage("assets/image/Ellipse 107.png"),
+                      ),
+
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    "Profile",
+                    style: TextStyle(
+                        fontSize: 22, fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+        ],
+      ),
+    );
+  }
+}
